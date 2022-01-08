2108212205851bdb819baca945a145ea80ee6252
Kristina <71969190+Kristina140699@users.noreply.github.com>
2022-01-08 19:30:09 +0530

    Add files via upload

---
+++ PHPcodes/55ajaxfetch2.php
@@ -0,0 +1,37 @@
+<?php
+$mysqli = new mysqli("localhost", "root", "", "custom");
+if($mysqli->connect_error) {
+exit('Could not connect');
+}
+// else{
+//     echo 'Connected! <br/>';
+// }
+$sql = "SELECT customerid, companyname, customername, cust_address, city, postalcode, country
+FROM customers WHERE customerid = ?";
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param("s", $_GET['q']);
+$stmt->execute();
+$stmt->store_result();
+$stmt->bind_result($cid, $cname, $name, $adr, $city, $pcode, $country);
+$stmt->fetch();
+$stmt->close();
+
+echo "<table>";
+echo "<tr>";
+echo "<th>CustomerID</th>";
+echo "<td>" . $cid . "</td>";
+echo "<th>CompanyName</th>";
+echo "<td>" . $cname . "</td>";
+echo "<th>CustomerName</th>";
+echo "<td>" . $name . "</td>";
+echo "<th>Address</th>";
+echo "<td>" . $adr . "</td>";
+echo "<th>City</th>";
+echo "<td>" . $city . "</td>";
+echo "<th>PostalCode</th>";
+echo "<td>" . $pcode . "</td>";
+echo "<th>Country</th>";
+echo "<td>" . $country . "</td>";
+echo "</tr>";
+echo "</table>";
+?>
